1c16fdfdd6fabb531ffafcc79c9cf79b5c17857f
ddobokki <ddobokki@naver.com>
2021-09-25 14:33:38 +0900

    피보나치 수 5

---
+++ 피보나치 수 5/hakyeom.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+fibo_seq = {0: 0,
+            1: 1}
+
+
+def fibonaci(idx):
+    if idx in fibo_seq.keys():
+        return fibo_seq[idx]
+    else:
+        fibo_seq[idx] = fibonaci(idx - 1) + fibonaci(idx - 2)
+        return fibo_seq[idx]
+
+
+print(fibonaci(n))
