d93fd31a206515f5dea7e2eaa98f5e5795f21586
20180707jun <20180707jun@gmail.com>
2021-09-26 20:43:19 +0900

    week1

--- 거스름돈/jy.py
+++ 거스름돈/jy.py
@@ -1,9 +1,10 @@
 n = int(input())
 f_coin = n//5
 
-while True:
+while f_coin>-1:
     if (n-f_coin*5)%2==0:
         print(f_coin+(n-5*f_coin)//2)
-        break
+        exit()
     else:
         f_coin -= 1
+print(-1)
