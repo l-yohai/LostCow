d6b245aa6d43b332ae06e5b46bb175bb0164bdd8
ddobokki <ddobokki@naver.com>
2021-11-20 10:54:28 +0900

    '22-29'

--- 문자열 집합/hakyeom.py
+++ 문자열 집합/hakyeom.py
@@ -4,17 +4,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-string_set = []
+string_set = {}
 for _ in range(n):
-    string_set.append(input().strip())
+    string_set[input().strip()]  = 0
 cnt = 0
 
-subset_list = []
 for _ in range(m):
     subset_string = input().strip()
-    if not subset_string in subset_list:
-        if subset_string in string_set:
-            cnt += 1
-            subset_list.append(subset_string)
+    if subset_string in string_set:
+        cnt += 1
 
 print(cnt)
